fe33dd3309e72412f68fa46e90a9d1649e1e29e1
gayfulina <irinmdr@gmail.com>
2021-12-16 10:54:50 -0800

    get

--- src/components/survey/UserExperiences.vue
+++ src/components/survey/UserExperiences.vue
@@ -3,14 +3,14 @@
     <base-card>
       <h2>Submitted Experiences</h2>
       <div>
-        <base-button>Load Submitted Experiences</base-button>
+        <base-button @click='getResult'>Load Submitted Experiences</base-button>
       </div>
       <ul>
         <survey-result
-          v-for="result in results"
-          :key="result.id"
-          :name="result.name"
-          :rating="result.rating"
+          v-for='result in results'
+          :key='result.id'
+          :name='result.name'
+          :rating='result.rating'
         ></survey-result>
       </ul>
     </base-card>
@@ -18,13 +18,58 @@
 </template>
 
 <script>
+import axios from 'axios';
 import SurveyResult from './SurveyResult.vue';
 
 export default {
-  props: ['results'],
   components: {
-    SurveyResult,
+    SurveyResult
   },
+  data() {
+    return {
+      results: []
+    };
+  },
+  methods: {
+    //   fetchResult() {
+    //     fetch('https://vue-http-demo-77e9e-default-rtdb.firebaseio.com/surveys.json')
+    //       .then((response) => {
+    //         if (response.ok) {
+    //           return response.json();
+    //         }
+    //       })
+    //       .then((data) => {
+    //         console.log(data);
+    //         const tempResults = [];
+    //         for (const id in data) {
+    //           tempResults.push({
+    //             id: id,
+    //             name: data[id].name,
+    //             rating: data[id].rating
+    //           });
+    //         }
+    //         this.results = tempResults;
+    //       });
+    //   }
+    // },
+
+    getResult() {
+      axios.get('https://vue-http-demo-77e9e-default-rtdb.firebaseio.com/surveys.json')
+        .then(res => {
+          console.log(res.data);
+          const tempResults = [];
+          for (const id in res.data) {
+            tempResults.push({
+              id: id,
+              name: res.data[id].name,
+              rating: res.data[id].rating
+            });
+          }
+          this.results = tempResults;
+        })
+        .catch(err => err);
+    }
+  }
 };
 </script>
 
